82fc5bda9516aae96d8be8fccf3072a11aa93a59
Lorena <lo.calfin@duocuc.cl>
2024-09-25 14:32:05 -0300

    cambiso

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  email: string = '';
+  username: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async showAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 
+  async register() {
+    if (!this.email || !this.username || !this.password) {
+      await this.showAlert('Por favor, completa todos los campos.');
+      return;
+    }
+
+    // Aquí puedes agregar el código para registrar al usuario
+    console.log('Usuario registrado:', { email: this.email, username: this.username, password: this.password });
+  }
 }
